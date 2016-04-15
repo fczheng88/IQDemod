a129d793b3455633e9193a92f343d3239141ee93
fczheng88 <fczheng88@gmail.com>
2016-04-15 00:57:36 -0500

    Fixed Plotting
    Plotting works a bit better now

--- IQDemod/IQCalc.java
+++ IQDemod/IQCalc.java
@@ -23,7 +23,7 @@ public class IQCalc
 
     private double sampleRate;
 
-    private double[] downSampledArr;
+    
 
     /**
      * Constructor for objects of class IQCalc
@@ -63,6 +63,9 @@ public class IQCalc
 
     public void demodulate()
     {
+        //I = decimate(I, sampleRate/2.5e6);
+        //Q = decimate(Q, sampleRate/2.5e6);
+        
         phaseAngles = new double[I.length];
         //calculate each phase angle
         for(int i = 0; i < phaseAngles.length; i++)
@@ -86,14 +89,13 @@ public class IQCalc
 
     public double[] unwrap(double[] u)
     {
-        int  k=0;				// initialize k to 0
+        int  k=0;               // initialize k to 0
         double[] yout = new double[u.length];
         for(int i = 0; i < u.length-1; i++)
         {
             yout[i]=u[i]+(2*Math.PI*k); // add 2*pi*k to ui
             if((Math.abs(u[i+1]-u[i]))>Math.PI) //if diff is greater than alpha, increment or decrement k
             {
-
                 if(u[i+1]<u[i])  // if the phase jump is negative, increment k
                 {
                     k=k+1;
@@ -108,16 +110,25 @@ public class IQCalc
         return yout;
     }
 
-    public void downSample(double targetRate)
+    public double[] decimate(double[] arr, double factor)
     {
-        int mult = (int)(sampleRate/targetRate);
-        downSampledArr = new double[paDerivs.length/mult];
+        double[] downSampledArr = new double[(int)(arr.length/factor)];
         for(int i = 0; i < downSampledArr.length; i++)
         {
-            downSampledArr[i] = paDerivs[i*mult];
+            downSampledArr[i] = arr[(int)(i*factor)];
         }
+        return downSampledArr;
     }
-
+    public int[] decimate(int[] arr, double factor)
+    {
+        int[] downSampledArr = new int[(int)(arr.length/factor)];
+        for(int i = 0; i < downSampledArr.length; i++)
+        {
+            downSampledArr[i] = arr[(int)(i*factor)];
+        }
+        return downSampledArr;
+    }
+    
     public void play()
     {
 
@@ -128,19 +139,16 @@ public class IQCalc
 
     public void show()
     {
-
         JFrame frame = new SoundClipFrame();
         frame.setVisible(true);
-
     }
 
     public static void main(String[] args)
     {
-        IQCalc calc = new IQCalc(2.5e6);
-        calc.readIQFile("capture.bin");
+        IQCalc calc = new IQCalc(1.8e6);
+        calc.readIQFile("capture2.bin");
         System.out.println(calc.getIntList().size());
         calc.demodulate();
-        calc.downSample(44.1e3);
 
         calc.show();
     }
@@ -155,7 +163,12 @@ public class IQCalc
         {
             frames++;
             offsets++;
-            double[] samples = downSampledArr;
+            double[] samples = decimate(paDerivs,sampleRate/2.5e6);
+            for(int i = 0; i < samples.length; i++)
+            {
+                samples[i] = samples[i]*100;
+            }
+            
 
             addWindowListener(new WindowAdapter()
                 {
@@ -178,7 +191,7 @@ public class IQCalc
                     {
                         int increment = samples.length / getWidth();
 
-                        final int LARGEST = 4;
+                        final int LARGEST = 250;
                         int x = 0;
                         for (int i = 0; i < samples.length; i = i + increment)
                         {
